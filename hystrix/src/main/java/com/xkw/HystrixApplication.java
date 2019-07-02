7abed5a5e1bd05a5317b94a13218fd6010d1c01e
lndlwangwei <lndl.wangwei@gmail.com>
2019-07-02 23:38:45 +0800

    add hystrix

--- hystrix/src/main/java/com/xkw/HystrixApplication.java
+++ hystrix/src/main/java/com/xkw/HystrixApplication.java
@@ -2,12 +2,25 @@ package com.xkw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableHystrix
+@EnableEurekaClient
 public class HystrixApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HystrixApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
